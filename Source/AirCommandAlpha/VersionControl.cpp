30c4eb82d8f521880d3918dc650fb6e4d654c15f
Steve Hardister <shardister@gmail.com>
2026-07-19 07:04:33 -0400

    Converted to a C++ Project

---
+++ Source/AirCommandAlpha/VersionControl.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VersionControl.h"
+
+VersionControl::VersionControl()
+{
+}
+
+VersionControl::~VersionControl()
+{
+}
